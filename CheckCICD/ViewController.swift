68c0d074e620c9a45d31f6bcb122e30c8654cece
haiphan <hai.phan@chotot.vn>
2024-10-15 16:21:27 +0700

    pod init

---
+++ CheckCICD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckCICD
+//
+//  Created by Hai Phan Thanh on 15/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
